791c65e2c89cf0d3b6d28ab096a93af5bec7bec3
marcomeisen <marcomeisen@gmail.com>
2019-08-11 10:39:09 +0200

    Android Studio Fix

--- build/frontend_server.d
+++ build/frontend_server.d
@@ -1,1 +1,1 @@
-frontend_server.d: /Users/marco.meisen/development/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
+frontend_server.d: /Users/marcomeisen/devtools/flutter/bin/cache/artifacts/engine/darwin-x64/frontend_server.dart.snapshot
